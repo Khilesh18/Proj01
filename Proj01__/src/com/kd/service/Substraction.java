3171a526ba59987a72754e18b74d9365571a343a
user <user@LAPTOP-950O6R5H>
2025-01-10 19:24:52 +0530

    Update Substraction

--- Proj01__/src/com/kd/service/Substraction.java
+++ Proj01__/src/com/kd/service/Substraction.java
@@ -5,6 +5,9 @@ public class Substraction {
 	public static void main(String[] args) {
 		
 		System.out.println("10-9=1");
+		System.out.println("20-9=11");
+		System.out.println("30-9=21");
+		System.out.println("40-9=31");
 
 	}
 
